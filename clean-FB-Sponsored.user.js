f5ae2e9c6c4950c122a2c68be2f8cb82eaffcda1
haogege <haogege@users.noreply.github.com>
2021-07-15 09:31:29 +0800

    Create clean-FB-Sponsored.user.js

---
+++ clean-FB-Sponsored.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Clean FB Sponsored
+// @namespace    https://github.com/haogege/ad
+// @version      0.1
+// @description  移除 FB 赞助内容
+// @author       haogege
+// @include      /https:\/\/www.facebook.com
+// @grant        none
+// @downloadURL  https://github.com/chgc/CleanFBSapce/raw/master/clean-FB-space.user.js
+// ==/UserScript==
+
+function removerightRailAd() {
+  var rr = document.querySelector("[data-pagelet='RightRail']");
+  if (rr != null && rr.children.length > 0) {
+    if (rr.children[0].children.length > 0) {
+      rr.children[0].children[0].style = 'display:none';
+    }
+  }
+}
+
+const NeedToRemoveKeywords = ['赞助内容', 'Sponsored'];
+
+function checkKeywordsExist(node) {
+  return NeedToRemoveKeywords.some((lang) => node.querySelector('[aria-label=' + lang + ']') != null);
+}
+
+function removeSponsorPost() {
+  document.querySelectorAll("div[data-pagelet*='FeedUnit_']").forEach((node) => {
+    if (checkKeywordsExist(node)) {
+      node.remove();
+    }
+  });
+}
+
+function executeActions() {
+  removerightRailAd();
+  removeSponsorPost();
+  setTimeout(executeActions, 200);
+}
+
+(function () {
+  'use strict';
+
+  // Your code here...
+  executeActions();
+})();
