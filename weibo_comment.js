8542da3b15ff480da343c3258e94babf44b5108c
haogege <haogege@users.noreply.github.com>
2022-10-20 10:08:35 +0800

    Update weibo_comment.js

--- weibo_comment.js
+++ weibo_comment.js
@@ -1,4 +1,4 @@
-function modify(body) {
+function JSON.parse($response.body) {
     if (body.hasOwnProperty('datas')) {
         if (body.datas.hasOwnProperty('type')) {
             body.datas.type != 1;
@@ -8,5 +8,6 @@ function modify(body) {
 }
 
 let body = $response.body;
+let body = JSON.parse($response.body);
 body = JSON.stringify(body);
 $done({body});
