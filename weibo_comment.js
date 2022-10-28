c47e26bba82d5bb27dc2a6145cbaad74349e15fe
haogege <haogege@users.noreply.github.com>
2022-10-28 16:19:42 +0800

    Update weibo_comment.js

--- weibo_comment.js
+++ weibo_comment.js
@@ -1,13 +1,15 @@
-function JSON.parse($response.body) {
-    if (body.hasOwnProperty('datas')) {
-        if (body.datas.hasOwnProperty('type')) {
-            body.datas.type != 1;
+function modify(url, body) {
+    if (url.includes("/comments/build_comments")) {
+        body = JSON.parse(body);
+        if (body.hasOwnProperty('datas')) {
+            body.datas.type === 1
         }
+        return JSON.stringify(body);
     }
     return body;
 }
 
+const url = $request.url;
 let body = $response.body;
-let body = JSON.parse($response.body);
-body = JSON.stringify(body);
-$done({body});
+body = modify(url, body);
+$done({ body: body });
