bf7d3fa72934a3a589a075bd37a09cf6ca3a40f9
yaohao <yaohao@qq.com>
2022-10-31 17:34:50 +0800

    1

--- weibo/weibo_comment.js
+++ weibo/weibo_comment.js
@@ -1,13 +1,14 @@
-//let body = $response.body;
+let body = $response.body;
 //body = JSON.parse(body);
-//body['datas'] = body['datas'].filter(element => !(element['type'] === 1));
-//body = JSON.stringify(body);
-//$done({body});
+body['datas'] = body['datas'].filter(element => !(element['type'] === 1));
+body = JSON.stringify(body);
+$done({body});
 
-
+/*
 let obj = JSON.parse($response.body);
 let resultData = obj["datas"].filter((item) => {
   return item["type"] != 1
 })
 obj["datas"] = resultData;
 $done({body:JSON.stringify(obj)});
+*/
