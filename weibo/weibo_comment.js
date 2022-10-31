7a9403ff15abfbde8c98b2f8fcf527918f3dcc73
yaohao <yaohao@qq.com>
2022-10-31 16:46:15 +0800

    1

--- weibo/weibo_comment.js
+++ weibo/weibo_comment.js
@@ -1,5 +1,5 @@
 let body = $response.body;
-body = JSON.parse(body);
+//body = JSON.parse(body);
 // 过滤评论
 // adType: '推荐/广告', type: 1
 body['datas'] = body['datas'].filter(element => !(element['type'] === 1));
