0998e84755fa266b512cac573a6305219b901273
haogege <haogege@users.noreply.github.com>
2022-10-31 15:12:06 +0800

    Update weibo_comment.js

--- weibo_comment.js
+++ weibo_comment.js
@@ -1,8 +1,7 @@
-//let obj = JSON.parse($response.body);
-let body = JSON.parse(data);
-let date = $response.body;
-// 过滤评论（评论中嵌套的回复详情界面也会用这个 api，但那里没有 datas）
+let body = $response.body;
+body = JSON.parse(body);
+// 过滤评论
 // adType: '推荐/广告', type: 1
-if (body.hasOwnProperty('datas'))
-    body['datas'] = body['datas'].filter(element => (element['type'] !== 1));
-$done({body: JSON.stringify(body)});
+body['datas'] = body['datas'].filter(element => !(element['type'] === 1));
+body = JSON.stringify(body);
+$done({body});
